7e2f48836b879769f5b755580da3b681bc60d32a
charlottehope <65370439+charlottehope@users.noreply.github.com>
2020-09-26 01:02:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// Question 1
+
+var firstVar;
+
+//Question 2
+
+var firstName = "Charlotte";
+
+//Question 3
+
+var myAge = 27;
+
+//Question 4
+
+var division = 20 / 5;
+
+//Question 5
+
+console.log(frog);
+
+//Question 6
+
+var orderHasShipped = true;
+
+//Question 7
+
+if(orderHasShipped === true)    {
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+//Question 8
+
+for(var count = 0; count <= 9; count++) {
+    console.log(count);
+}
